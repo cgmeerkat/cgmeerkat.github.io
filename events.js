a987115798ed305b7aaf5f7b94261eed26f4b0f8
CG Meerkat <cg.meerkat@outlook.com>
2024-07-16 21:34:19 -0500

    Initial commit

---
+++ events.js
@@ -0,0 +1,13 @@
+function trackButtonClick(label) {
+    gtag('event', 'click', {
+        'event_category': 'button',
+        'event_label': label,
+    });
+}
+
+function trackLinkClick(label) {
+    gtag('event', 'click', {
+        'event_category': 'link',
+        'event_label': label,
+    });
+}
